39111ad3749547b8e312e79b40c27614820559d8
wesleysouza <wesley.souza@outlook.com>
2015-12-02 14:35:34 -0300

    Create Symbol.java
    Classe auxiliar para as varáveis.

---
+++ Symbol.java
@@ -0,0 +1,30 @@
+
+public class Symbol {
+    
+    private String tipo;
+    private String valor;
+    
+    public Symbol(String tipo, String valor) {
+        // TODO Auto-generated constructor stub
+        this.setTipo(tipo);
+        this.setValor(valor);
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+    
+    
+}
